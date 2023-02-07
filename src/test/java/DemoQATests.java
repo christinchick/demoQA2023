fd91fd5ed11ae5892ab6473e80c64ce3ae4d7ddf
christinchick <alexey@skyward-crimea.ru>
2023-02-07 13:11:15 +0300

    Creat tests

--- src/test/java/DemoQATests.java
+++ src/test/java/DemoQATests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,11 +15,12 @@ import static com.codeborne.selenide.Selenide.open;
 public class DemoQATests {
     @BeforeAll
     static void setUp(){
-        Configuration.browserSize="1923x1080";
+        Configuration.browserSize="1800x1700";
     }
     @Test
     void homeworkone(){
         open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("#firstName").setValue("Kristina");
         $("#lastName").setValue("Karabestkaia");
         $("#userEmail").setValue("Kristina@skyward.com");
@@ -28,12 +30,17 @@ public class DemoQATests {
         $(".react-datepicker__month-select").selectOption("June");
         $(".react-datepicker__year-select").selectOption("1991");
         $(".react-datepicker__day--027:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Hindi").pressEnter();
         $("#hobbies-checkbox-3").parent().click();
-        $("uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
         $("#currentAddress").setValue("Simferopol").scrollTo();
         $("#state").click();
-        $("#state").$(byText("NCR")).click();
+        $("#state").$(byText("Haryana")).click();
         $("#city").click();
+        $("#city").$(byText("Karnal")).scrollTo().click();
+        $("#submit").scrollTo().click();
+        $("#submit").scrollTo().click();
 
 
 
